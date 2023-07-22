7e714073c1f333aab613b32d486f8e13228491a3
Leo1412 <69981435+Leo1412@users.noreply.github.com>
2023-07-22 22:42:00 +0800

    first try

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -10,6 +10,19 @@ public class TestArrayRingBuffer {
     @Test
     public void someTest() {
         //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> x = new ArrayRingBuffer(4);
+        x.enqueue(1);
+        x.enqueue(2);
+        x.enqueue(3);
+        x.enqueue(4);
+        assertTrue(x.isFull());
+        assertTrue(x.dequeue() == 1);
+        assertTrue(x.dequeue() == 2);
+        assertTrue(x.dequeue() == 3);
+        assertTrue(x.dequeue() == 4);
+        //suppoesd to throw an exception
+        x.dequeue();
+
     }
 
     /** Calls tests for ArrayRingBuffer. */
--- hw1/synthesizer/TestGuitarString.java
+++ hw1/synthesizer/TestGuitarString.java
@@ -2,6 +2,7 @@ package synthesizer;
 
 /* Since this test is part of a package, we have to import the package version of StdAudio. */
 /* Don't worry too much about this, we'll get there in due time. */
+
 import edu.princeton.cs.introcs.StdAudio;
 
 import org.junit.Test;
@@ -12,7 +13,7 @@ import static org.junit.Assert.*;
  */
 
 public class TestGuitarString {
-/*    @Test
+    @Test
     public void testPluckTheAString() {
         double CONCERT_A = 440.0;
         GuitarString aString = new GuitarString(CONCERT_A);
@@ -21,9 +22,9 @@ public class TestGuitarString {
             StdAudio.play(aString.sample());
             aString.tic();
         }
-    }*/
+    }
 
-/*
+
     @Test
     public void testTic() {
         // Create a GuitarString of frequency 11025, which
@@ -52,7 +53,7 @@ public class TestGuitarString {
         assertEquals(expected, s5, 0.001);
 
     }
-*/
+
 
     /** Calls tests for GuitarString. */
     public static void main(String[] args) {
